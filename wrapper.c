82341d41ac649c2e820ff92032022d9c055214db
KJH198 <kjh15071687155@126.com>
2025-03-23 19:15:59 +0800

    修复wrapper.c

--- wrapper.c
+++ wrapper.c
@@ -412,6 +412,6 @@ char* fdt_get_property_string(void *fdt, const char *path, const char *prop, int
     return NULL;
   }
   // 获取这个property的值，并返回
-  const char *ret = fdt_getprop(fdt, node, prop, len);
+  char *ret = fdt_getprop(fdt, node, prop, len);
   return ret;
 }
